992d8a54b8329f9422afca6910e0ff7bd0f20613
John W. Fail <john@pixelache.ac>
2018-01-19 14:49:50 +0200

    more translations yeah yeah

--- app/mailers/group_mailer.rb
+++ app/mailers/group_mailer.rb
@@ -1,12 +1,12 @@
 class GroupMailer < ActionMailer::Base
-  default from: "admin@temporary.fi"
+  default from: "info@kuusipalaa.fi"
 
 
   def new_member(group, member)
     recipient = member.user
     @group = group
     @user = recipient
-    unless recipient =~ /^change@me/ || recipient.opt_in != true
+    unless recipient.email =~ /^change@me/ || recipient.opt_in != true
       mail(to: recipient.email,  subject: "You have been added to the group #{@group.display_name}")
     end
 
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,23 +1,23 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "admin@temporary.fi"
-  
- 
+  default from: "info@kuusipalaa.fi"
+
+
   def new_comment(item, comment, recipient)
     @item = item
     @comment = comment
     @user = recipient
-    unless recipient =~ /^change@me/ || recipient.opt_in != true
-      mail(to: recipient.email,  subject: "New comment on: #{@item.name}") 
+    unless recipient.email =~ /^change@me/ || recipient.opt_in != true
+      mail(to: recipient.email,  subject: "New comment on: #{@item.name}")
     end
-    
+
   end
-  
+
   def new_pledge(item, pledge, recipient)
     @item = item
     @pledge = pledge
     @user = recipient
     unless recipient =~ /^change@me/ || recipient.opt_in != true
-      mail(to: recipient.email,  subject: "New pledge to proposal: #{@item.name}") 
+      mail(to: recipient.email,  subject: "New pledge to proposal: #{@item.name}")
     end
   end
 
@@ -26,9 +26,9 @@ class NotificationMailer < ActionMailer::Base
     @item = item
     @user = recipient
     unless recipient =~ /^change@me/ || recipient.opt_in != true
-      mail(to: recipient.email,  subject: "A new meeting of #{@item.event.name} has been scheduled") 
+      mail(to: recipient.email,  subject: "A new meeting of #{@item.event.name} has been scheduled")
     end
-    
+
   end
 
 end
