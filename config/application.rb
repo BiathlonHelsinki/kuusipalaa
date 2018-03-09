f70cf6372f59f974750acf5c2d59aca2e7b273ed
John W. Fail <john@pixelache.ac>
2018-03-09 22:53:30 +0200

    allow map iframe

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Experiment2
     config.load_defaults 5.1
     config.time_zone = 'Helsinki'
     config.assets.precompile += %w[404.html 422.html 500.html]
+    config.action_view.sanitized_allowed_tags = ['strong', 'em', 'a', 'br', 'iframe']
+  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
