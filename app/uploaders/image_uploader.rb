13f888f585b18eb6e8b2b62b9c25484046d0cdb7
John W. Fail <john@pixelache.ac>
2018-04-28 15:27:09 +0300

    fix image orientations

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,27 +16,41 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :wide do
+    process :auto_orient
     process :resize_to_fill => [1200, 500]
   end
 
   version :twelvehundred do
+    process :auto_orient
     process :resize_to_fit => [1200, 900]
   end
 
-  version :medium do
+  version :medium, from: :twelvehundred do
+    process :auto_orient
     process :resize_to_fit => [800, 600]
   end
 
-  version :small do
+  version :small, from: :medium do
+    process :auto_orient
     process :resize_to_fit => [300, 300]
   end
 
   version :box do
+    process :auto_orient
     process :resize_to_fill => [400, 400]
   end
 
-  version :thumb do
+  version :thumb, from: :box do
+    process :auto_orient
     process :resize_to_fill => [100, 100]
   end
+
+  def auto_orient
+    manipulate! do |img|
+      img = img.auto_orient
+    end
+  end
+
+
 
 end
