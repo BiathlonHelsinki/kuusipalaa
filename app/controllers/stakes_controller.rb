5610a95490861532cd0b2e74b3a5a131c3f4453a
John W. Fail <john@pixelache.ac>
2018-06-16 09:47:11 +0300

    ending

--- app/controllers/stakes_controller.rb
+++ app/controllers/stakes_controller.rb
@@ -3,30 +3,31 @@ class StakesController < ApplicationController
 
 
   def create
-    @season = Season.find(params[:season_id])
-    @stake = Stake.new(stake_params)
-    if @stake.save
-      StakeMailer.new_stake(@stake).deliver_now
-      flash[:notice] = t(:stake_booked_invoice_later)
-      redirect_to season_stake_path(@season, @stake)
-    else
+    render template: 'stakes/no_stakes'
+    # @season = Season.find(params[:season_id])
+    # @stake = Stake.new(stake_params)
+    # if @stake.save
+    #   StakeMailer.new_stake(@stake).deliver_now
+    #   flash[:notice] = t(:stake_booked_invoice_later)
+    #   redirect_to season_stake_path(@season, @stake)
+    # else
 
-      flash[:error] = t(:error_booking_stake) + " : " + @stake.errors.messages.values.join('; ')
-      fill_collection
-      render template: 'stakes/new'
-    end
+    #   flash[:error] = t(:error_booking_stake) + " : " + @stake.errors.messages.values.join('; ')
+    #   fill_collection
+    #   render template: 'stakes/new'
+    # end
   end
 
 
   def for_self
-
-    @season = Season.find(params[:season_id])
-    @stake = Stake.new(season: @season, bookedby: current_user)
-    if !current_user.is_member? &&  params[:accepted_agreement] != 'true'
-      redirect_to members_agreement_users_path
-    elsif !current_user.has_membership_details?
-      redirect_to get_membership_details_user_path(current_user)
-    end
+    render template: 'stakes/no_stakes'
+    # @season = Season.find(params[:season_id])
+    # @stake = Stake.new(season: @season, bookedby: current_user)
+    # if !current_user.is_member? &&  params[:accepted_agreement] != 'true'
+    #   redirect_to members_agreement_users_path
+    # elsif !current_user.has_membership_details?
+    #   redirect_to get_membership_details_user_path(current_user)
+    # end
   end
 
   def for_group
@@ -49,33 +50,34 @@ class StakesController < ApplicationController
 
   def new
 
-    @season = Season.find(params[:season_id])
-    @stake = Stake.new(season: @season, bookedby: current_user)
+    # @season = Season.find(params[:season_id])
+    # @stake = Stake.new(season: @season, bookedby: current_user)
 
-    if params[:group_id]
-      @group = Group.friendly.find(params[:group_id])
-      if @group.taxid.blank? && !current_user.is_member?
-        flash[:notice] = t(:you_must_become_member)
-        redirect_to members_agreement_users_path and return
-      else
-        fill_collection
-        if @group.members.map(&:user).include?(current_user)
-          if @group.members.find_by(user: current_user).access_level > KuusiPalaa::Access::REGULAR_MEMBER
-            # @stake.price = @group.is_member ? 60.48 : (@group.taxid.blank? ? 40.32 : 100)
-            @stake.price = @group.is_member ? 75 : (@group.taxid.blank? ? 50 : 124)
-            render template: 'stakes/group_stakes'
-          else
-            flash[:error] = t(:must_be_admin_to_buy_stakes)
-          end
-        else
-          flash[:error] = t(:must_be_admin_to_buy_stakes)
-        end
-      end
+    # if params[:group_id]
+    #   @group = Group.friendly.find(params[:group_id])
+    #   if @group.taxid.blank? && !current_user.is_member?
+    #     flash[:notice] = t(:you_must_become_member)
+    #     redirect_to members_agreement_users_path and return
+    #   else
+    #     fill_collection
+    #     if @group.members.map(&:user).include?(current_user)
+    #       if @group.members.find_by(user: current_user).access_level > KuusiPalaa::Access::REGULAR_MEMBER
+    #         # @stake.price = @group.is_member ? 60.48 : (@group.taxid.blank? ? 40.32 : 100)
+    #         @stake.price = @group.is_member ? 75 : (@group.taxid.blank? ? 50 : 124)
+    #         render template: 'stakes/group_stakes'
+    #       else
+    #         flash[:error] = t(:must_be_admin_to_buy_stakes)
+    #       end
+    #     else
+    #       flash[:error] = t(:must_be_admin_to_buy_stakes)
+    #     end
+    #   end
 
-    else
-      fill_collection
-      render template: 'stakes/new'
-    end
+    # else
+    #   fill_collection
+    #   render template: 'stakes/new'
+    # end
+    render template: 'stakes/no_stakes'
   end
 
   def show
